7d328f47674129fe0026d7889ab7d18d56c12082
Murat GUZEL <guzelmurat@gmail.com>
2012-02-24 01:18:00 +0200

    facebook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :index
   
   def index 
-    # ana sayfa
+    
   end   
   
   def main    
-    # üyenin girdiği sayfa
+   
   end
 end

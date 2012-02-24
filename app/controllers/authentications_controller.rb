e36da62dc3442cde6cc9ed019e1c819bb3ff01d4
Murat GUZEL <guzelmurat@gmail.com>
2012-02-24 11:37:12 +0200

    custom authentication

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -7,15 +7,19 @@ class AuthenticationsController < ApplicationController
      authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
      if authentication 
      else
-        User.transaction do 
-          user = User.new
-          user.password = Devise.friendly_token[0,20]
-          user.email = "guzelmurat@gmail.com"
-          user.authentications.build(:provider => auth['provider'], :uid => auth['uid'])
-          user.save!       
-        end         
-        flash[:notice] = "Kullanıcı giriş yaptı."
-        sign_in_and_redirect(:user, user)
+        user = User.new
+        user.password = Devise.friendly_token[0,20]   
+        if auth['provider'] == "facebook"            
+          user.email = auth['extra']['raw_info'].email           
+        end
+        user.authentications.build(:provider => auth['provider'], :uid => auth['uid'])
+        if user.save
+          flash[:notice] = "Kullanıcı giriş yaptı."
+          sign_in_and_redirect(:user, user)                
+        else
+          session[:omniauth] = auth.except('extra')
+          redirect_to new_user_registration_path
+        end
      end
      
     

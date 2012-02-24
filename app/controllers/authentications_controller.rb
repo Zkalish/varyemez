be143b63f5c603712e436731e3ac78419f5a84f3
Murat GUZEL <guzelmurat@gmail.com>
2012-02-24 11:26:07 +0200

    tweet test

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,9 +1,23 @@
+#encoding:utf-8
 class AuthenticationsController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :create    
-  
+                     
   def create
-     render :text => request.env["omniauth.auth"].to_yaml
-  end
-  
-  
+     auth = request.env["omniauth.auth"]              
+     authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
+     if authentication 
+     else
+        User.transaction do 
+          user = User.new
+          user.password = Devise.friendly_token[0,20]
+          user.email = "guzelmurat@gmail.com"
+          user.authentications.build(:provider => auth['provider'], :uid => auth['uid'])
+          user.save!       
+        end         
+        flash[:notice] = "Kullanıcı giriş yaptı."
+        sign_in_and_redirect(:user, user)
+     end
+     
+    
+  end    
 end

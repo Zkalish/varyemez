761f8d3a4a68dd07c6cdc1c445373a66ab6f9a46
Murat GUZEL <guzelmurat@gmail.com>
2012-02-24 02:00:00 +0200

    twitter authentication

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,13 +13,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end 
   
   def twitter
-    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user, email)
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
     email = "info@egebilgi.com.tr"
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end   
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,12 +25,12 @@ class User < ActiveRecord::Base
     end
   end 
   
-  def self.find_for_twitter_oauth(access_token, signed_in_resource=nil, email)
+  def self.find_for_twitter_oauth(access_token, signed_in_resource=nil)
     data = access_token.extra.raw_info
-    if user = User.where(:email => email).first
+    if user = User.where(:email => data.email).first
       user
     else # Create a user with a stub password. 
-      User.create!(:email => email, :password => Devise.friendly_token[0,20]) 
+      User.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
     end
   end    
                                                                                                                          
